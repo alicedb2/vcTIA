3321386b30a5ac65aa04607ea5376beab2acfb13
Alice Doucet Beaupré <alice.doucet.beaupre@gmail.com>
2022-08-02 21:33:16 -0400

    cleanup

--- definitions.h
+++ definitions.h
@@ -3,11 +3,12 @@
 #define LATCH_PIN 4     // yellow (RCLK)
 #define CLOCK_PIN 5     // orange (SCLK)
 
-#define ACTRL0 0b0101
-#define ACTRL1 0b0110
-#define AFREQ0 0b0111
-#define AFREQ1 0b1000
-#define  AVOL0 0b1001
-#define  AVOL1 0b1010
+#define WAVEFORM_POT A0
+#define PITCH_POT A1
 
-#define DELAY 0
+#define ADDR_CTRL0 0b0101
+#define ADDR_CTRL1 0b0110
+#define ADDR_FREQ0 0b0111
+#define ADDR_FREQ1 0b1000
+#define ADDR_VOL0 0b1001
+#define ADDR_VOL1 0b1010
--- vcTIA.ino
+++ vcTIA.ino
@@ -1,7 +1,7 @@
 #include "definitions.h"
 
-int last_wf = 0;
-int last_pitch = 0;
+int last_wf = -1;
+int last_pitch = -1;
 
 void setup()
 {
@@ -10,73 +10,60 @@ void setup()
   pinMode(DATA_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
 
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
+  pinMode(WAVEFORM_POT, INPUT);
+  pinMode(PITCH_POT, INPUT);
 
   Serial.begin(115200);
   Serial.flush();
 
-  setPitch(2, 10);
+  setWaveform(2, 0);
+  setPitch(2, 0);
   setVolume(2, 2);
-  
-}
 
+//  demo();
+}
 
 void loop()
 {
 
-  int wf_read = analogRead(A0);
+  int wf_read = analogRead(WAVEFORM_POT);
   int wf = map(wf_read, 0, 1024, 0, 16);
 
-  int pitch_read = analogRead(A1);
+  int pitch_read = analogRead(PITCH_POT);
   int pitch = map(pitch_read, 0, 1024, 0, 32);
-  
+
   if (last_wf != wf) {
     last_wf = wf;
     setWaveform(2, wf);
+    Serial.print("Waveform "); Serial.println(wf);
   }
 
   if (last_pitch != pitch) {
     last_pitch = pitch;
     setPitch(2, pitch);
+    Serial.print("Pitch "); Serial.println(30000/(pitch + 1));
   }
-//  Serial.println("foo");
-//  delay(100);
-  
-//  setVolume(2, 3);   // 0-15
-//  setPitch(2, 30);   // 0-31
-//  setWaveform(0, 4); // 0-15
-//  setWaveform(1, 7); // 0-15
-////  delay(1000);
 
-//  setVolume(2, 3);
-//  setWaveform(2, 0);
-//  for (int wf = 0; wf < 16; wf++) {
-//    Serial.print("Waveform ");
-//    Serial.print(wf);
-//    Serial.print("  pitch ");
-//    setWaveform(2, wf);
-//    for (int pt = 0; pt < 32; pt++) {
-//      Serial.print(pt);
-//      Serial.print(".");
-//      setPitch(2, pt);
-//      delay(100);
-//    }
-//    Serial.println();
-//  }
-//  
-//  setWaveform(2, 0);
-//  setPitch(2, 10);
-//  for (int wf = 0; wf < 16; wf++) {
-//    setWaveform(2, wf);
-//    delay(100);
-//  }
-//  setWaveform(2, 0);
-//  
-//  delay(1000000);
+}
 
-  //  updateShiftRegister(0b00000000, 0b11000111);
-  //  delay(100000);
+void demo()
+{
+  setVolume(2, 2);
+  setWaveform(2, 0);
+  for (int wf = 0; wf < 16; wf++) {
+    Serial.print("Waveform ");
+    Serial.print(wf);
+    Serial.print("  pitch ");
+    setWaveform(2, wf);
+    for (int pt = 0; pt < 32; pt++) {
+      Serial.print(pt);
+      Serial.print(".");
+      setPitch(2, pt);
+      delay(100);
+    }
+    Serial.println();
+  }
+  setWaveform(2, 0);
 }
 
 /*
@@ -94,22 +81,20 @@ void latchTIA()
 {
   digitalWrite(TIA_LATCH_PIN, LOW);
   digitalWrite(TIA_LATCH_PIN, HIGH);
-  delay(10);
+  delay(1);
   digitalWrite(TIA_LATCH_PIN, LOW);
-//  delay(DELAY);
-
 }
 
 // Volume goes from 0b0000 = 0 to 0b1111 = 15
 void setVolume(int voice, byte volume)
 {
   if (voice == 0 || voice == 2) {
-    updateShiftRegister(volume, AVOL0);
+    updateShiftRegister(volume, ADDR_VOL0);
     latchTIA();
   }
 
   if (voice == 1 || voice == 2) {
-    updateShiftRegister(volume, AVOL1);
+    updateShiftRegister(volume, ADDR_VOL1);
     latchTIA();
   }
 
@@ -119,12 +104,12 @@ void setVolume(int voice, byte volume)
 void setPitch(int voice, int divisor)
 {
   if (voice == 0 || voice == 2) {
-    updateShiftRegister(divisor, AFREQ0);
+    updateShiftRegister(divisor, ADDR_FREQ0);
     latchTIA();
   }
 
   if (voice == 1 || voice == 2) {
-    updateShiftRegister(divisor, AFREQ1);
+    updateShiftRegister(divisor, ADDR_FREQ1);
     latchTIA();
   }
 }
@@ -133,39 +118,12 @@ void setPitch(int voice, int divisor)
 void setWaveform(int voice, int waveform)
 {
   if (voice == 0 || voice == 2) {
-    updateShiftRegister(waveform, ACTRL0);
+    updateShiftRegister(waveform, ADDR_CTRL0);
     latchTIA();
   }
-  delay(1);
+
   if (voice == 1 || voice == 2) {
-    updateShiftRegister(waveform, ACTRL1);
+    updateShiftRegister(waveform, ADDR_CTRL1);
     latchTIA();
   }
 }
-
-
-
-//void input_and_play_semitone() {
-//  static String inData;
-//
-//  Serial.println("");
-//  for (;;) {
-//    char received = ' '; // Each character received
-//    inData = ""; // Clear recieved buffer
-//    Serial.print("DAC value (0-4095): ");
-//
-//    while (received != '\n') { // When new line character is received (\n = LF, \r = CR)
-//      if (Serial.available() > 0) // When character in serial buffer read it
-//      {
-//        received = Serial.read();
-//        Serial.print(received); // Echo each received character, terminal dont need local echo
-//        inData += received; // Add received character to inData buffer
-//      }
-//    }
-//    inData.trim(); // Eliminate \n, \r, blank and other not “printable”
-//    Serial.println();
-//    MCP.setValue(inData.toInt());
-//
-//  }
-//
-//}
